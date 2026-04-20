191acc3e40d9f85b475f9dd8602e53a414498cd3
7-Tarun <tarunghadoria890@gmail.com>
2026-04-20 10:46:43 +0530

    fixing small bugs on random.js

--- 09_DOM/5_Basics/2_Random.js
+++ 09_DOM/5_Basics/2_Random.js
@@ -1,5 +1,5 @@
 //Formula of generating random number:  Math.random()+Math.floor()
-// Math.random()- O aur 1 ke beech se randim number deta hai. Like-0.37,0.29.
+// Math.random()- O aur 1 ke beech se random number deta hai. Like-0.37,0.29.
 // Math.floor()- Number ko roundfigure me karta hai. Like- 3.9 ko 3.
 
 let num = Math.random()*5;      //Giving random number between 0-5.
