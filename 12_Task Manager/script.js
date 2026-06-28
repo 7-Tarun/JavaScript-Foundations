937c7f74fa4740394adce886109885120d457bf6
7-Tarun <tarunghadoria890@gmail.com>
2026-06-28 12:04:22 +0530

    a renderTasks function created and reduced the code complexity and Improving code architecture

--- 12_Task Manager/script.js
+++ 12_Task Manager/script.js
@@ -9,84 +9,90 @@ let searchInput = document.querySelector("#searchInput");
 let inputList = [];
 let completedCount = 0;
 
+function renderTasks() {
+    list.innerHTML = "";
+
+    inputList.forEach(function (task) {
+        let taskList = document.createElement("li");
+        taskList.textContent = task;
+
+        //Complete Button created in front of li.
+        let completeBtn = document.createElement("button");
+        completeBtn.textContent = "Complete";
+        taskList.appendChild(completeBtn);
+        completeBtn.classList.add("comBtn");
+
+        //Delete Button created in front of li.
+        let deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        taskList.appendChild(deleteBtn);
+        deleteBtn.classList.add("delBtn");
+
+        list.appendChild(taskList);
+
+        completeBtn.addEventListener("click", function () {
+            if (taskList.classList.contains("completed")) {
+                return;
+            }
+
+            taskList.classList.add("completed");
+            completeBtn.classList.add("comBtnUpdate");
+            deleteBtn.classList.add("delBtnUpdate");
+            completeBtn.textContent = "Done";
+
+            //Task Counter
+            completedCount++;
+            completedTask.textContent = completedCount;
+            pendingTask.textContent = inputList.length - completedCount;
+        });
+
+        deleteBtn.addEventListener("click", function () {
+            if (taskList.classList.contains("completed")) {
+                completedCount--;
+                completedTask.textContent = completedCount;
+                pendingTask.textContent = inputList.length - completedCount;
+            }
+
+            //Task Counter
+            inputList.length = inputList.length - 1;
+            totalTask.textContent = inputList.length;
+            pendingTask.textContent = inputList.length - completedCount;
+
+            taskList.remove();
+        });
+    });
+}
+
 addBtn.addEventListener("click", function () {
     let arr = input.value.trim();
-
-    //Li Element created and with input text content.
-    let taskList = document.createElement("li");
-    taskList.textContent = arr;
-
-    //Complete Button created in front of li.
-    let completeBtn = document.createElement("button");
-    completeBtn.textContent = "Complete";
-    taskList.appendChild(completeBtn);
-    completeBtn.classList.add("comBtn");
-
-    //Delete Button created in front of li.
-    let deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-    taskList.appendChild(deleteBtn);
-    deleteBtn.classList.add("delBtn");
 
     if (arr === "") {
         alert("Enter A Task!")
     }
-    else if(inputList.includes(arr)){
+    else if (inputList.includes(arr)) {
         alert("Task already exists!");
     }
     else {
-        list.appendChild(taskList);
         inputList.push(arr);
-        totalTask.textContent = inputList.length;
+        renderTasks();
 
-        //Task Counter
+        totalTask.textContent = inputList.length;
         pendingTask.textContent = inputList.length;
 
         input.value = "";
     }
-
-    completeBtn.addEventListener("click", function () {
-        if (taskList.classList.contains("completed")) {
-            return;
-        }
-
-        taskList.classList.add("completed");
-        completeBtn.classList.add("comBtnUpdate");
-        deleteBtn.classList.add("delBtnUpdate");
-        completeBtn.textContent = "Done";
-
-        //Task Counter
-        completedCount++;
-        completedTask.textContent = completedCount;
-        pendingTask.textContent = inputList.length - completedCount;
-    });
-
-    deleteBtn.addEventListener("click", function () {
-        if(taskList.classList.contains("completed")){
-            completedCount--;
-            completedTask.textContent = completedCount;
-            pendingTask.textContent = inputList.length - completedCount;
-        }
-
-        //Task Counter
-        inputList.length = inputList.length - 1;
-        totalTask.textContent = inputList.length;
-        pendingTask.textContent = inputList.length - completedCount;
-
-        taskList.remove();
-    });
 });
 
-searchInput.addEventListener("input",function(){
+searchInput.addEventListener("input", function () {
     let value = searchInput.value.toLowerCase();
     let items = document.querySelectorAll("#lists li");
 
     items.forEach(items => {
         let text = items.firstChild.textContent.toLowerCase();
-        if(text.includes(value)){
+        if (text.includes(value)) {
             items.style.display = "flex";
         }
-        else{
+        else {
             items.style.display = "none";
         }
     });
@@ -94,15 +100,15 @@ searchInput.addEventListener("input",function(){
 
 
 //Keyboard Enter Support
-input.addEventListener('keydown',(event) => {
-    if(event.key === "Enter"){
+input.addEventListener('keydown', (event) => {
+    if (event.key === "Enter") {
         let arr = input.value.trim();
 
-        if(arr !== "") {
+        if (arr !== "") {
             addBtn.click();
             input.value = "";
         }
-        else{
+        else {
             alert("Enter A Task!");
         }
     }
