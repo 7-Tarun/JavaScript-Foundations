dc93c13d83ada6f14747dc8d2058ca09c755aeeb
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-01 13:51:17 +0530

    Click Count practice project with better architecture

--- Rough.js
+++ Rough.js
@@ -1,29 +1,51 @@
-let count = 0;      //state
+let count = 0;
 
 const button = document.querySelector("#addBtn");
 const output = document.querySelector("#count");
 
-button.addEventListener("click", function () {
+button.addEventListener("click", manager);
+
+function manager() {
+    const newCount = incrementCount(count);
     
-});
+    updateState(newCount);
+    
+    const isLimitReached = checkLimit(count);
+    const isCountEven = isEven(count);
+    
+    saveCount(count);
+    renderUi(count, isLimitReached, isCountEven);
+}
 
+function incrementCount(currentCount) {
+    return currentCount + 1;
+}
 
-// USER ACTION
-//     ↓
-// Event Listener
-//     ↓
-// Manager
-//     ↓
-// incrementCount(count)     ← Pure
-//     ↓
-// newCount
-//     ↓
-// count = newCount          ← State update
-//     ↓
-// checkLimit(count)         ← Pure
-//     ↓
-// isEven(count)             ← Pure
-//     ↓
-// saveCount(count)          ← Side effect
-//     ↓
-// renderUI(count, ...)      ← Side effect
+function checkLimit(currentCount) {
+    return currentCount >= 10;
+}
+
+function isEven(currentCount) {
+    return currentCount % 2 === 0;
+}
+
+function updateState(newCount) {
+    count = newCount;
+}
+
+function saveCount(currentCount) {
+    localStorage.setItem("savedCount", currentCount);
+}
+
+function renderUi(currentCount, isLimit, isEvenNo) {
+    if (isLimit) {
+        output.textContent = `Maximum Limit Reached (${currentCount})`;
+        return;
+    }
+    
+    if (isEvenNo) {
+        output.textContent = `${currentCount} — Even`;
+    } else {
+        output.textContent = `${currentCount} — Odd`;
+    }
+}
