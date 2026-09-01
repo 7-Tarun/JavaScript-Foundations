230c6d71b1664c60c1d3621b76f95111e8691098
7-Tarun <214530696+7-Tarun@users.noreply.github.com>
2026-09-01 12:08:30 +0530

    Problem identified and better architecture created

--- Rough.js
+++ Rough.js
@@ -1,24 +1,29 @@
-let count = 0;
+let count = 0;      //state
 
 const button = document.querySelector("#addBtn");
 const output = document.querySelector("#count");
 
 button.addEventListener("click", function () {
-
-    count++;
-
-    if (count > 10) {
-        output.textContent = "Limit reached!";
-        localStorage.setItem("limitReached", "true");
-        return;
-    }
-
-    if (count % 2 === 0) {
-        output.textContent = `${count} — Even`;
-    } else {
-        output.textContent = `${count} — Odd`;
-    }
-
-    localStorage.setItem("count", count);
-
+    
 });
+
+
+// USER ACTION
+//     ↓
+// Event Listener
+//     ↓
+// Manager
+//     ↓
+// incrementCount(count)     ← Pure
+//     ↓
+// newCount
+//     ↓
+// count = newCount          ← State update
+//     ↓
+// checkLimit(count)         ← Pure
+//     ↓
+// isEven(count)             ← Pure
+//     ↓
+// saveCount(count)          ← Side effect
+//     ↓
+// renderUI(count, ...)      ← Side effect
